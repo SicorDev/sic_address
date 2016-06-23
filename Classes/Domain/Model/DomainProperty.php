adf0389885a8f541b56bdef758fd148440eda332
SicorDev <dev@sicor-kdl.net>
2016-06-23 14:23:05 +0000

    Added Getter and Setter

--- Classes/Domain/Model/DomainProperty.php
+++ Classes/Domain/Model/DomainProperty.php
@@ -74,4 +74,88 @@ class DomainProperty extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      */
     protected $isListLabel;
 
+    /**
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    /**
+     * @return SICOR\SicAddress\Domain\Model\FieldType
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * @param SICOR\SicAddress\Domain\Model\FieldType $type
+     */
+    public function setType($type) {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel($label) {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTcaOverride() {
+        return $this->tcaOverride;
+    }
+
+    /**
+     * @param string $tcaOverride
+     */
+    public function setTcaOverride($tcaOverride) {
+        $this->tcaOverride = $tcaOverride;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSettings() {
+        return $this->settings;
+    }
+
+    /**
+     * @param string $settings
+     */
+    public function setSettings($settings) {
+        $this->settings = $settings;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIsListLabel() {
+        return $this->isListLabel;
+    }
+
+    /**
+     * @param boolean $isListLabel
+     */
+    public function setIsListLabel($isListLabel) {
+        $this->isListLabel = $isListLabel;
+    }
+
 }
--- Classes/Domain/Model/FieldType.php
+++ Classes/Domain/Model/FieldType.php
@@ -38,4 +38,19 @@ class FieldType extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      * @var string
      */
     protected $title = "";
+
+    /**
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+    
 }

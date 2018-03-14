396088f1dc095641c6c66dd2981b18e5f8b803f0
mrotschkar <mr@sicor-kdl.net>
2018-03-14 09:04:29 +0100

    MR: Set Version number to 1.0.5.

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.4',
+  'version' => '1.0.5',
   'constraints' =>
   array (
     'depends' =>

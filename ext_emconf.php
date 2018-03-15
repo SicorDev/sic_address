fce0e075c0c9ed88435daf83d59af01ed3fa8457
mrotschkar <mr@sicor-kdl.net>
2018-03-15 08:16:27 +0100

    MR: Forgot version number again... :-)

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.5',
+  'version' => '1.0.6',
   'constraints' =>
   array (
     'depends' =>

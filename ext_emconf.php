360a55c451883cdcbf70a8d1f8ac726080cf04a6
mrotschkar <mr@sicor-kdl.net>
2018-07-16 14:21:13 +0200

    AZ: Added l10n_state field to generator, so DB analyzer is not required.

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.1.3',
+  'version' => '1.1.4',
   'constraints' =>
   array (
     'depends' =>

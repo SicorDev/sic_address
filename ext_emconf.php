ef913a9c808619f476076ad58725f2784370dd26
Zadnan <github@zadnan.de>
2019-01-11 17:47:56 +0100

    [BACKPORT] restoring compatibility for 7.6 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,8 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '8.7.0-9.5.99',
-      'php' => 7
+      'typo3' => '7.6.0-9.5.99',
     ),
     'conflicts' => 
     array (

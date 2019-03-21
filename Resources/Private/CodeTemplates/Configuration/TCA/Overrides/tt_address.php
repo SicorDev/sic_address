46e7794be8b65e7776f6aca4ec5ab55c184ebab9
Adnan Zelkanovic <az@sicor-kdl.net>
2019-03-21 17:26:05 +0100

    [UPDATE] generated tt_address configuration only works now, if tt_address extension is active. So system wont crash anymore when tt_address override configuration is generated and tt_address is not available.

--- Resources/Private/CodeTemplates/Configuration/TCA/Overrides/tt_address.php
+++ Resources/Private/CodeTemplates/Configuration/TCA/Overrides/tt_address.php
@@ -1,6 +1,9 @@
 <?php
-// Add additional columns to tt_address TCA
-$tmp_sic_address_columns = array(
+if(!empty($GLOBALS['TCA']['tt_address'])) {
+
+    // Add additional columns to tt_address TCA
+    $tmp_sic_address_columns = array(
+<f:spaceless>
      <f:for each="{properties}" as="field">
      <f:if condition="{field.external} == '0'">
      <f:format.htmlentitiesDecode>
@@ -13,48 +16,52 @@ $tmp_sic_address_columns = array(
      </f:format.htmlentitiesDecode>
      </f:if>
     </f:for>
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_address',$tmp_sic_address_columns);
+</f:spaceless>
+    );
 
-// Extend show items from tt_address TCA
-$GLOBALS['TCA']['tt_address']['types']['0']['showitem'] .= ',--div--;LLL:EXT:sic_address/Resources/Private/Language/locallang_db.xlf:sic_address_tca_tab_label,';
-$GLOBALS['TCA']['tt_address']['types']['0']['showitem'] .= '<f:for each="{properties}" as="field"><f:if condition="{field.external} == '0'"> {field.title},</f:if></f:for>';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_address',$tmp_sic_address_columns);
 
-// Upgrade Description to an RTE field
-$GLOBALS['TCA']['tt_address']['columns']['description']['defaultExtras'] = 'richtext[*]';
-$GLOBALS['TCA']['tt_address']['columns']['description']['config']['wizards'] = array(
-    'RTE' => array(
-        'icon' => 'wizard_rte2.gif',
-        'notNewRecords' => 1,
-        'RTEonly' => 1,
-        'module' => array(
-            'name' => 'wizard_rich_text_editor',
-            'urlParameters' => array(
-                'mode' => 'wizard',
-                'act' => 'wizard_rte.php'
-            )
-        ),
-        'title' => 'LLL:EXT:cms/locallang_ttc.xlf:bodytext.W.RTE',
-        'type' => 'script'
-    )
-);
+    // Extend show items from tt_address TCA
+    $GLOBALS['TCA']['tt_address']['types']['0']['showitem'] .= ',--div--;LLL:EXT:sic_address/Resources/Private/Language/locallang_db.xlf:sic_address_tca_tab_label,';
+    $GLOBALS['TCA']['tt_address']['types']['0']['showitem'] .= '<f:for each="{properties}" as="field"><f:if condition="{field.external} == '0'"> {field.title},</f:if></f:for>';
 
-// Set company field as label, name and email as alternative label, ordering by company
-$GLOBALS['TCA']['tt_address']['ctrl']['label'] = 'company';
-$GLOBALS['TCA']['tt_address']['ctrl']['label_alt'] = 'name, email';
-$GLOBALS['TCA']['tt_address']['ctrl']['default_sortby'] = 'ORDER BY company';
+    // Upgrade Description to an RTE field
+    $GLOBALS['TCA']['tt_address']['columns']['description']['defaultExtras'] = 'richtext[*]';
+    $GLOBALS['TCA']['tt_address']['columns']['description']['config']['wizards'] = array(
+        'RTE' => array(
+            'icon' => 'wizard_rte2.gif',
+            'notNewRecords' => 1,
+            'RTEonly' => 1,
+            'module' => array(
+                'name' => 'wizard_rich_text_editor',
+                'urlParameters' => array(
+                    'mode' => 'wizard',
+                    'act' => 'wizard_rte.php'
+                )
+            ),
+            'title' => 'LLL:EXT:cms/locallang_ttc.xlf:bodytext.W.RTE',
+            'type' => 'script'
+        )
+    );
 
-// Add company to backend search
-$GLOBALS['TCA']['tt_address']['ctrl']['searchFields'] .= ', company';
+    // Set company field as label, name and email as alternative label, ordering by company
+    $GLOBALS['TCA']['tt_address']['ctrl']['label'] = 'company';
+    $GLOBALS['TCA']['tt_address']['ctrl']['label_alt'] = 'name, email';
+    $GLOBALS['TCA']['tt_address']['ctrl']['default_sortby'] = 'ORDER BY company';
 
-// Fix tt_address bug: https://github.com/FriendsOfTYPO3/tt_address/issues/55
-$GLOBALS['TCA']['tt_address']['columns']['birthday']['config'] = array(
-    'type' => 'input',
-    'renderType' => 'inputDateTime',
-    'eval' => 'date',
-    'size' => '8',
-    'default' => 0
-);
+    // Add company to backend search
+    $GLOBALS['TCA']['tt_address']['ctrl']['searchFields'] .= ', company';
 
-// Link to sys_category
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable('sic_address', 'tt_address');
+    // Fix tt_address bug: https://github.com/FriendsOfTYPO3/tt_address/issues/55
+    $GLOBALS['TCA']['tt_address']['columns']['birthday']['config'] = array(
+        'type' => 'input',
+        'renderType' => 'inputDateTime',
+        'eval' => 'date',
+        'size' => '8',
+        'default' => 0
+    );
+
+    // Link to sys_category
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable('sic_address', 'tt_address');
+
+}

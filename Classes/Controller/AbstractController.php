abc13f530ecd5b0792e2aeb323495552a42cbe04
Adnan Zelkanovic <az@sicor-kdl.net>
2020-03-05 11:15:32 +0100

    [TASK] some code styling

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -2,16 +2,20 @@
 namespace SICOR\SicAddress\Controller;
 
 class AbstractController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+
 
     /**
      * Returns translation for given key (and extension)
      *
      * @param [type] $key
      * @param string $extension
-     * @return void
+     * @return string
      */
     protected function translate($key, $extension = 'sic_address') {
-        return \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate($key, $extension );
+        return LocalizationUtility::translate($key, $extension );
+    }
+
     }
 
 }

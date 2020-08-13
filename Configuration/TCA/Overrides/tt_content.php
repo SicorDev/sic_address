17c067cab1900be5cb3b368c64cc8370ebed290f
bvogel <bv@sicor-kdl.net>
2020-08-13 17:03:19 +0200

    [Update] Overrides for Content and TypoScript templates added

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,16 @@
+<?php
+defined('TYPO3_MODE') or die('Access denied.');
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+ExtensionUtility::registerPlugin(
+    'sic_address',
+    'Sicaddress',
+    'LLL:EXT:sic_address/Resources/Private/Language/locallang_sicaddress.xlf:mlang_tabs_tab'
+);
+
+$pluginSignature = 'sicaddress_sicaddress';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:sic_address/Configuration/FlexForms/flexform_sicaddresslist.xml');

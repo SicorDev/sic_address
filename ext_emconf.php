cd1968136b4448f067e85f707772bf35874cabd0
MR <mr@sicor-kdl.net>
2021-11-30 13:34:25 +0100

    MR: Version 3.1.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array (
   'state' => 'stable',
   'uploadfolder' => false,
   'clearCacheOnLoad' => 0,
-  'version' => '3.1.4',
+  'version' => '3.1.5',
   'constraints' =>
   array (
     'depends' =>

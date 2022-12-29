9b5e15be0e0a46363b9141ecdf304d410f0c76b5
MR <mr@sicor.de>
2022-12-29 15:06:27 +0100

    MR: Jeez...

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
         array(
             'depends' =>
                 array(
-                    'typo3' => '11.5.*',
+                    'typo3' => '11.5.0-11.5.99',
                     'php' => '7.4.0-8.1.99',
                 ),
             'suggests' =>

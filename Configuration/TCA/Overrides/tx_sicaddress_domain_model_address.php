670cbafec4dbbf8c2f319779f5d1326ab71ff9e9
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-29 13:01:19 +0200

    Deprecation no. 85613 - Category Registry

--- Configuration/TCA/Overrides/tx_sicaddress_domain_model_address.php
+++ Configuration/TCA/Overrides/tx_sicaddress_domain_model_address.php
@@ -2,21 +2,29 @@
 
 use SICOR\SicAddress\Utility\Service;
 
-if (!class_exists(SICOR\SicAddress\Utility\Service::class)) {
-    $path = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sic_address');
-    require_once($path . 'Classes/Utility/Service.php');
-}
+call_user_func(function($extensionKey, $table)
+{
+    if (!class_exists(SICOR\SicAddress\Utility\Service::class)) {
+        $path = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sic_address');
+        require_once($path . 'Classes/Utility/Service.php');
+    }
 
-$extensionConfiguration = Service::getConfiguration();
+    $extensionConfiguration = Service::getConfiguration();
 
-if (!$extensionConfiguration["ttAddressMapping"]) {
-    // Only required when tt_address is not used as address table.
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable('sic_address', 'tx_sicaddress_domain_model_address');
-}
+    if (
+        !$extensionConfiguration["ttAddressMapping"]
+    ) {
+        $GLOBALS['TCA'][$table]['columns']['categories'] = [
+            'config' => [
+                'type' => 'category',
+                'foreign_table_where' => ' AND sys_category.sys_language_uid IN (-1, 0) ORDER BY sys_category.title'
+            ]
+        ];
+    }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_sicaddress_domain_model_address', 'EXT:sic_address/Resources/Private/Language/locallang_csh_tx_sicaddress_domain_model_address.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sicaddress_domain_model_address');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, 'categories');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr($table, 'EXT:' . $extensionKey . '/Resources/Private/Language/locallang_csh_' . $table . '.xlf');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages($table);
+}, 'sic_address', basename(__FILE__, '.php'));
 
-if (isset($GLOBALS['TCA']['tx_sicaddress_domain_model_address']['columns']['categories'])) {
-    $GLOBALS['TCA']['tx_sicaddress_domain_model_address']['columns']['categories']['config']['foreign_table_where'] = ' AND sys_category.sys_language_uid IN (-1, 0) ORDER BY sys_category.title';
-}
+

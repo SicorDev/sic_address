1d977bc39b1442778702d5822a2271da389f3fc2
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-29 20:08:02 +0200

    use call_user_function with paramters extensionKey and extensionName

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,40 +2,43 @@
 
 defined('TYPO3') or die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'SicAddress',
-    'Sicaddress',
-    [
-        \SICOR\SicAddress\Controller\AddressController::class => 'list, show, new, create, edit, update, delete, search'
-    ],
-    [
-        \SICOR\SicAddress\Controller\AddressController::class => 'create, update, delete, search'
-    ]
-);
+call_user_func(function($extensionName)
+{
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        $extensionName,
+        'Sicaddress',
+        [
+            \SICOR\SicAddress\Controller\AddressController::class => 'list, show, new, create, edit, update, delete, search'
+        ],
+        [
+            \SICOR\SicAddress\Controller\AddressController::class => 'create, update, delete, search'
+        ]
+    );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'SicAddress',
-    'SicaddressVianovisExport',
-    [
-        \SICOR\SicAddress\Controller\ExportController::class => 'exportVianovis',
-    ],
-    []
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        $extensionName,
+        'SicaddressVianovisExport',
+        [
+            \SICOR\SicAddress\Controller\ExportController::class => 'exportVianovis',
+        ],
+        []
+    );
 
-// Register used signals
-$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-$signalSlotDispatcher->connect(
-    \TYPO3\CMS\Extensionmanager\Service\ExtensionManagementService::class,
-    'hasInstalledExtensions',
-    \SICOR\SicAddress\Domain\Service\SignalService::class,
-    'afterExtensionInstall',
-    false
-);
+    // Register used signals
+    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+    $signalSlotDispatcher->connect(
+        \TYPO3\CMS\Extensionmanager\Service\ExtensionManagementService::class,
+        'hasInstalledExtensions',
+        \SICOR\SicAddress\Domain\Service\SignalService::class,
+        'afterExtensionInstall',
+        false
+    );
 
-$signalSlotDispatcher->connect(
-    'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
-    'afterExtensionConfigurationWrite',
-    \SICOR\SicAddress\Domain\Service\SignalService::class,
-    'refreshModuleList',
-    false
-);
+    $signalSlotDispatcher->connect(
+        'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
+        'afterExtensionConfigurationWrite',
+        \SICOR\SicAddress\Domain\Service\SignalService::class,
+        'refreshModuleList',
+        false
+    );
+}, 'SicAddress');

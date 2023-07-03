983cc63a839be93dcac21b89e81da2bc1e27faf2
MR <mr@sicor.de>
2023-07-03 11:44:15 +0200

    MR: Added Getters/Setters for uid and hidden.

--- Resources/Private/CodeTemplates/Classes/Domain/Model/Address.php
+++ Resources/Private/CodeTemplates/Classes/Domain/Model/Address.php
@@ -33,23 +33,32 @@ class Address extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 
     /**
     * categories
-    *
     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\SICOR\SicAddress\Domain\Model\Category>
     */
     protected $categories = null;
 
     /**
      * Sortfield
-     *
      * @var String
      */
     protected $sortField;
+
+    /**
+     * uid
+     * @var int
+     */
+    protected $uid;
+
+    /**
+     * hidden
+     * @var boolean
+     */
+    protected $hidden;
 
     <f:for each="{properties}" as="property">
     <f:format.htmlentitiesDecode>
     /**
      * {property.title}
-     *
      * @var {property.type.modelType}
      */
     protected ${property.title};
@@ -155,5 +164,34 @@ class Address extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
      */
     public function setSortField($sortField) {
         $this->sortField = $sortField;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUid() {
+        return $this->uid;
+    }
+
+    /**
+     * @param int $uid
+     * @return void
+     */
+    public function setUid($uid) {
+        $this->uid = $uid;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHidden() {
+        return $this->hidden;
+    }
+
+    /**
+     * @param bool $hidden
+     */
+    public function setHidden($hidden) {
+        $this->hidden = $hidden;
     }
 }

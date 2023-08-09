ba7d02a6034844bbbb8b44bf22a507fa6e94e220
Franz Holzinger <franz.holzinger@festwein.de>
2023-08-09 22:04:30 +0200

    replace removed signal slot dispatcher by event listener

---
+++ Configuration/Icons.php
@@ -0,0 +1,13 @@
+<?php
+   return [
+       // Icon identifiers
+       'extension' => [
+           'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+           // The source bitmap file
+           'source' => 'EXT:sic_address/Resources/Public/Icons/Extension.gif',
+       ],
+       'module24' => [
+           'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+           'source' => 'EXT:sic_address/Resources/Public/Icons/module_icon_24',
+       ],
+   ];

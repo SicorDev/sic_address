2070cec3d203d7e08994242475fd44155acfd8ec
MR <mr@sicor.de>
2023-10-11 16:32:52 +0200

    MR: Remove calls to getFileFieldTCAConfig

--- Configuration/TCA/Overrides/sys_category.php
+++ Configuration/TCA/Overrides/sys_category.php
@@ -7,27 +7,20 @@ $newSysCategoryColumns = array(
     'sic_address_marker' => array(
         'exclude' => true,
         'label' => 'LLL:EXT:sic_address/Resources/Private/Language/locallang.xlf:sys_category_marker_icon',
-        'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
-            'sic_address_marker',
-            array(
-                'behaviour' => [
-                    'allowLanguageSynchronization' => true,
-                ],
-                'appearance' => [
-                    'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference',
-                    'showPossibleLocalizationRecords' => true,
-                    'showRemovedLocalizationRecords' => true,
-                    'showAllLocalizationLink' => true,
-                    'showSynchronizationLink' => true
-                ],
-                'foreign_match_fields' => [
-                    'fieldname' => 'sic_address_marker',
-                    'tablenames' => 'sys_category',
-                    'table_local' => 'sys_file',
-                ],
-            ),
-            $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
-        )
+        'config' => [
+            'type' => 'file',
+            'behaviour' => [
+                'allowLanguageSynchronization' => true,
+            ],
+            'appearance' => [
+                'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference',
+                'showPossibleLocalizationRecords' => true,
+                'showRemovedLocalizationRecords' => true,
+                'showAllLocalizationLink' => true,
+                'showSynchronizationLink' => true
+            ],
+            'allowed' => 'common-image-types',
+        ],
     ),
 );
 

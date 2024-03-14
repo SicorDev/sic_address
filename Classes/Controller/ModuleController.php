68d8b1886f8e96004a632f14859710619f575a5d
MR <mr@sicor.de>
2024-03-14 12:02:36 +0100

    Fix: Don't touch tt_address hidden field.

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -422,16 +422,13 @@ class ModuleController extends AbstractController
 
             $fields = $this->addressRepository->getFields();
             foreach ($fields as $field) {
-                if (preg_match("/^(uid|pid|cruser_id|t3.*|tstamp|deleted|categories|sorting)$/", $field) === 0) {
+                if (preg_match("/^(uid|pid|cruser_id|t3.*|tstamp|deleted|categories|sorting|hidden)$/", $field) === 0) {
                     $domainProperty = new DomainProperty();
                     $domainProperty->setTitle($field);
                     $domainProperty->setTcaLabel($field);
                     switch ($field) {
                         case 'image':
                             $domainProperty->setType('image');
-                            break;
-                        case 'hidden':
-                            $domainProperty->setType('boolean');
                             break;
                         default:
                             $domainProperty->setType('string');
